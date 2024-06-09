60f1da371f67c91c20c254a376e646856a4d02dc
Samuel <fatu.samuel24@gmail.com>
2024-06-09 19:22:20 +0300

    Create TokenConstants.cs

---
+++ DiscordNetKitTesting/TokenConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace DiscordNetKitTesting;
+
+public class TokenConstants
+{
+    public const string BotToken = "[YOUR_BOT_TOKEN]";
+
+    public const string UserToken = "[YOUR_USER_TOKEN]";
+}
